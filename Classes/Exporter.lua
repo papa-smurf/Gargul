6312122480457553d5435e03c598796467d0109d
Kevin Op den Kamp <k.opdenkamp@gmail.com>
2021-07-13 21:27:04 +0200

    The exporter now allows you to select a date for which you'd like to export awared loot

--- Classes/Exporter.lua
+++ Classes/Exporter.lua
@@ -1,14 +1,57 @@
 local _, App = ...;
 
 App.Ace.GUI = App.Ace.GUI or LibStub("AceGUI-3.0");
-App.Exporter = {};
+App.Ace.ScrollingTable = App.Ace.ScrollingTable or LibStub("ScrollingTable");
+App.Exporter = {
+    visible = false,
+    dateSelected = nil,
+    UIComponents = {
+        Frame = nil,
+        Tables = {},
+        EditBoxes = {},
+    },
+};
 
 local Utils = App.Utils;
 local AceGUI = App.Ace.GUI;
+local ScrollingTable = App.Ace.ScrollingTable;
 local Exporter = App.Exporter;
+
+-- Add a resut table confirmation dialog to Blizzard's global StaticPopupDialogs object
+StaticPopupDialogs[App.name .. "_RESET_AWARD_HISTORY_CONFIRMATION"] = {
+    preferredIndex = 3,
+    text = "Are you sure you want to reset your reward history table? This cannot be undone!",
+    button1 = "Yes",
+    button2 = "No",
+    OnAccept = function()
+        App.DB.AwardHistory = {};
+        Exporter:close();
+        Exporter:draw();
+    end,
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+}
 
 function Exporter:draw()
     Utils:debug("Exporter:draw");
+
+    if (Exporter.visible) then
+        return;
+    end
+
+    Exporter.visible = true;
+
+    -- Fetch award history per date
+    local AwardHistoryByDate = {};
+    for _, AwardEntry in pairs(App.DB.AwardHistory) do
+        local date = date('%Y-%m-%d', AwardEntry.timestamp);
+        local Entries = Utils:tableGet(AwardHistoryByDate, date, {});
+
+        tinsert(Entries, AwardEntry);
+
+        AwardHistoryByDate[date] = Entries;
+    end
 
     -- Create a container/parent frame
     local ExportFrame = AceGUI:Create("Frame");
@@ -18,33 +61,152 @@ function Exporter:draw()
     ExportFrame:SetLayout("Flow");
     ExportFrame:SetWidth(600);
     ExportFrame:SetHeight(450);
+    ExportFrame:SetCallback("OnClose", function(widget)
+       Exporter:close();
+   end);
     ExportFrame.statustext:GetParent():Hide(); -- Hide the statustext bar
 
-    local exportString = "dateTime,character,itemID";
-    for _, AwardEntry in pairs(App.DB.AwardHistory) do
-        exportString = string.format("%s\n%s,%s,%s",
-            exportString,
-            date('%Y-%m-%d', AwardEntry.timestamp),
-            AwardEntry.awardedTo,
-            AwardEntry.itemId
-        );
-    end
+    Exporter.UIComponents.Frame = ExportFrame;
+
+    --[[
+        DATES FRAME
+    ]]
+    local DateFrame = AceGUI:Create("SimpleGroup");
+    DateFrame:SetLayout("FILL")
+    DateFrame:SetWidth(200);
+    DateFrame:SetHeight(350);
+    ExportFrame:AddChild(DateFrame);
+
+    -- Generate the characters table and add it to DateFrame.frame
+    Exporter:drawDatesTable(ExportFrame.frame, Utils:tableFlip(AwardHistoryByDate));
 
     -- Large edit box
     local ExportBox = AceGUI:Create("MultiLineEditBox");
-    ExportBox:SetText(exportString);
-    ExportBox:HighlightText();
-    ExportBox:SetFocus();
-    ExportBox:SetFullWidth(true);
+    ExportBox:SetText("");
+    ExportBox:SetWidth(360);
+    ExportBox:SetHeight(350);
     ExportBox:DisableButton(true);
     ExportBox:SetLabel("");
     ExportBox:SetNumLines(22);
     ExportBox:SetMaxLetters(999999999);
     ExportFrame:AddChild(ExportBox);
+
+    Exporter.UIComponents.EditBoxes.Export = ExportBox;
+
+    --[[
+        FOOTER BUTTON PARENT FRAME
+    ]]
+    local FooterFrame = AceGUI:Create("SimpleGroup");
+    FooterFrame:SetLayout("Flow");
+    FooterFrame:SetFullWidth(true);
+    FooterFrame:SetHeight(50);
+    ExportFrame:AddChild(FooterFrame);
+
+    local ClearButton = AceGUI:Create("Button");
+    ClearButton:SetText("Clear");
+    ClearButton:SetWidth(140);
+    ClearButton:SetCallback("OnClick", function()
+        StaticPopup_Show(App.name .. "_RESET_AWARD_HISTORY_CONFIRMATION");
+    end);
+    FooterFrame:AddChild(ClearButton);
+
+    Exporter:refreshExportString();
+end
+
+function Exporter:refreshExportString()
+    Utils:debug("Exporter:refreshExportString");
+
+    local exportString = "dateTime,character,itemID";
+
+    for _, AwardEntry in pairs(App.DB.AwardHistory) do
+        local dateString = date('%Y-%m-%d', AwardEntry.timestamp);
+
+        if (not Exporter.dateSelected or dateString == Exporter.dateSelected) then
+            exportString = string.format("%s\n%s,%s,%s",
+                exportString,
+                dateString,
+                AwardEntry.awardedTo,
+                AwardEntry.itemId
+            );
+        end
+    end
+
+    Exporter.UIComponents.EditBoxes.Export:SetText(exportString);
+end
+
+-- Close the exported
+function Exporter:close()
+    Utils:debug("Exporter:close");
+
+    if (not Exporter.visible) then
+        return;
+    end
+
+    -- Clear the frame and its widgets
+    AceGUI:Release(Exporter.UIComponents.Frame);
+    Exporter.visible = false;
+
+    -- Clean up the Dates table seperately
+    Exporter.UIComponents.Tables.Dates:SetData({}, true);
+    Exporter.UIComponents.Tables.Dates:Hide();
+end
+
+function Exporter:drawDatesTable(parent, Dates)
+    Utils:debug("Exporter:drawDatesTable");
+
+    local columns = {
+        {
+            name = "Date",
+            width = 120,
+            align = "LEFT",
+            color = {
+                r = 0.5,
+                g = 0.5,
+                b = 1.0,
+                a = 1.0
+            },
+            colorargs = nil,
+            sort = App.Data.Constants.ScrollingTable.descending,
+        },
+    };
+
+    local table = ScrollingTable:CreateST(columns, 21, 15, nil, parent);
+    table:EnableSelection(true);
+    table:SetWidth(120);
+    table.frame:SetPoint("BOTTOMLEFT", parent, "BOTTOMLEFT", 50, 78);
+
+    table:RegisterEvents({
+        ["OnClick"] = function (rowFrame, cellFrame, data, cols, row, realrow, column, scrollingTable, ...)
+
+            -- Even if we're still missing an answer from some of the group members
+            -- we still want to make sure our inspection end after a set amount of time
+            App.Ace:ScheduleTimer(function ()
+                Exporter.dateSelected = nil;
+                local Selected = table:GetRow(table:GetSelection());
+
+                if (Selected and Selected[1]) then
+                    Exporter.dateSelected = Selected[1];
+                end
+
+                Exporter:refreshExportString();
+            end, .1);
+        end
+    });
+
+    TableData = {};
+    for _, date in pairs(Dates) do
+        tinsert(TableData, {date});
+    end
+
+    -- The second argument refers to "isMinimalDataformat"
+    -- For the full format see https://www.wowace.com/projects/lib-st/pages/set-data
+    table:SetData(TableData, true);
+
+    self.UIComponents.Tables.Dates = table;
 end
 
 function Exporter:drawDkpExport()
-    Utils:debug("Exporter:draw");
+    Utils:debug("Exporter:drawDkpExport");
 
     -- Create a container/parent frame
     local ExportFrame = AceGUI:Create("Frame");
