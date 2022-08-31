b08c22347d60d7079380f9d97a0870abbcda7fdf
Kevin Op den Kamp <k.opdenkamp@gmail.com>
2022-08-31 16:47:22 +0200

    Trivial: renamed "Trade Expiration Timers" to "Gargul Trade Timers"

--- Interface/TradeWindow/TimeLeft.lua
+++ Interface/TradeWindow/TimeLeft.lua
@@ -92,7 +92,7 @@ function TimeLeft:draw()
 
     local Title = Window:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall");
     Title:SetPoint("TOPLEFT", 3, -3);
-    Title:SetText("Trade expiration timers");
+    Title:SetText("Gargul Trade Timers");
     Title:SetTextColor(1, 1, 1, 1);
 
     local Cogwheel = CreateFrame("Button", "TimeLeft_Cogwheel" .. GL:uuid(), Window, Frame);
