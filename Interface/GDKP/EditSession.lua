3c077475844b417dc32846e6cc1fe950d978bcfe
Kevin Op den Kamp <k.opdenkamp@gmail.com>
2022-11-21 12:11:25 +0100

    Added additional assets to be used in the GDKP module

---
+++ Interface/GDKP/EditSession.lua
@@ -0,0 +1,141 @@
+---@type GL
+local _, GL = ...;
+
+GL.AceGUI = GL.AceGUI or LibStub("AceGUI-3.0");
+local AceGUI = GL.AceGUI;
+
+---@class EditSession
+GL.Interface.GDKP.EditSession = {
+    isVisible = false,
+};
+
+---@type EditSession
+local EditSession = GL.Interface.GDKP.EditSession;
+
+---@type GDKPOverview
+local Overview = GL.Interface.GDKP.Overview;
+
+---@type Interface
+local Interface = GL.Interface;
+
+---@type GDKP
+local GDKP = GL.GDKP;
+
+---@return Frame
+function EditSession:build()
+    GL:debug("Interface.GDKP.EditSession:build");
+
+    local Window = AceGUI:Create("InlineGroup");
+    Window:SetLayout("Flow");
+    Window:SetWidth(200);
+    Window:SetHeight(280);
+    Window.frame:SetFrameStrata("HIGH");
+    Window.frame:Show();
+    Interface:set(self, "Window", Window);
+
+    local Label = GL.AceGUI:Create("Label");
+    Label:SetHeight(20);
+    Label:SetFullWidth(true);
+    Label:SetText("Editing");
+    Window:AddChild(Label);
+    Interface:set(self, "Title", Label);
+
+    local Title = GL.AceGUI:Create("EditBox");
+    Title:DisableButton(true);
+    Title:SetHeight(20);
+    Title:SetFullWidth(true);
+    Title:SetLabel("Name");
+    Window:AddChild(Title);
+    Interface:set(self, "Title", Title);
+
+    local Save = AceGUI:Create("Button");
+    Save:SetText("Save");
+    Save:SetFullWidth(true);
+    Save:SetCallback("OnClick", function()
+        local title = strtrim(Title:GetText());
+
+        if (GL:empty(title)) then
+            return;
+        end
+
+        GL:xd("EDIT SESSION!")
+
+        self:close();
+    end);
+    Window:AddChild(Save);
+
+    local Cancel = AceGUI:Create("Button");
+    Cancel:SetText("Cancel");
+    Cancel:SetFullWidth(true);
+    Cancel:SetCallback("OnClick", function()
+        self:close();
+    end);
+    Window:AddChild(Cancel);
+
+    return Window;
+end
+
+---@return Frame
+function EditSession:window()
+    GL:debug("Interface.GDKP.EditSession:window");
+
+    local Window = Interface:get(self, "Window");
+
+    if (not Window) then
+        Window = self:build();
+    end
+
+    return Window;
+end
+
+--- Toggle the create session window that's anchored to the right side of the GDKP overview window
+---
+---@return void
+function EditSession:toggle()
+    GL:debug("Interface.GDKP.EditSession:toggle");
+
+    if (self.isVisible) then
+        return self:close();
+    end
+
+    self:open();
+end
+
+---@return void
+function EditSession:open()
+    GL:debug("Interface.GDKP.EditSession:open");
+
+    local Session = GDKP:getSessionByID(Overview.selectedSession);
+
+    if (not Session) then
+        return;
+    end
+
+    -- It seems our GDKP overview window is not opened
+    if (not Overview.isVisible) then
+        return;
+    end
+
+    local Window = self:window();
+
+    Interface:get(self, "Label.Title"):SetText("Editing " .. Session.title);
+    Interface:get(self, "EditBox.Title"):SetValue(Session.title);
+
+    self.isVisible = true;
+    Window.frame:SetPoint("TOPLEFT", Interface:get(Overview, "GDKPOverview").frame, "TOPRIGHT", 2, 16);
+    Window.frame:Show();
+end
+
+---@return void
+function EditSession:close()
+    GL:debug("Interface.GDKP.EditSession:close");
+
+    local Window = self:window();
+
+    if (self.isVisible) then
+        Window.frame:Hide();
+        self.isVisible = false;
+    end
+end
+
+GL:debug("Interface.GDKP.EditSession.lua");
