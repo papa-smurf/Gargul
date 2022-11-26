96a889115f38f59a62e1140987ca67fccca72502
Kevin Op den Kamp <k.opdenkamp@gmail.com>
2022-11-26 13:47:34 +0100

    Updated the _ide_helper file to aid with development

--- _ide_helper.lua
+++ _ide_helper.lua
@@ -2,6 +2,202 @@
 GL = {} ---@type GL
 GL.Interface = GL.Interface or {}; ---@type Interface
 
+---@class AceGUIFrame
+AceGUIFrame = {};
+
+---@class AceGUISimpleGroup
+AceGUISimpleGroup = {};
+
+---@class AceGUILabel
+AceGUILabel = {};
+
+---@class AceGUIEditBox
+AceGUIEditBox = {};
+
+---@class AceGUIMultiLineEditBox
+AceGUIMultiLineEditBox = {};
+
+---@class AceGUIDropdown
+AceGUIDropdown = {};
+
+---@class AceGUIIcon
+AceGUIIcon = {};
+
+-- [[ AceGUIFrame ]]
+---@param value string
+function AceGUIFrame:SetStatusText(value) end
+
+---@param Child table
+function AceGUIFrame:AddChild(Child) end
+
+function AceGUIFrame:Hide() end
+function AceGUIFrame:Show() end
+function AceGUIFrame:PauseLayout() end
+function AceGUIFrame:DoLayout() end
+
+---@type Frame
+AceGUIFrame.frame = Frame;
+
+---@type AceGUISimpleGroup
+AceGUIFrame.statustext = {};
+
+-- [[ AceGUISimpleGroup ]]
+---@param Child table
+function AceGUISimpleGroup:AddChild(Child) end
+
+function AceGUISimpleGroup:Hide() end
+function AceGUISimpleGroup:Show() end
+
+function AceGUIFrame:PauseLayout() end
+function AceGUIFrame:DoLayout() end
+
+---@return AceGUIFrame
+function AceGUISimpleGroup:GetParent() end
+
+-- [[ AceGUILabel ]]
+
+---@param value string
+function AceGUILabel:SetText(value) end
+
+---@param FontObject table GameFontNormal GameFontNormalSmall GameFontNormalLarge GameFontHighlight GameFontHighlightSmall GameFontHighlightSmallOutline GameFontHighlightLarge GameFontDisable GameFontDisableSmall GameFontDisableLarge GameFontGreen GameFontGreenSmall GameFontGreenLarge GameFontRed GameFontRedSmall GameFontRedLarge GameFontWhite GameFontDarkGraySmall NumberFontNormalYellow NumberFontNormalSmallGray QuestFontNormalSmall DialogButtonHighlightText ErrorFont TextStatusBarText CombatLogFont function
+function AceGUILabel:SetFontObject(FontObject) end
+
+---@param trueOrFalse boolean
+function AceGUILabel:SetFullWidth(trueOrFalse) end
+
+---@return AceGUIFrame
+function AceGUILabel:GetParent() end
+
+function AceGUILabel:Hide() end
+function AceGUILabel:Show() end
+
+function AceGUILabel:ClearAllPoints(); end
+
+---@type Frame
+AceGUILabel.frame = Frame;
+
+-- [[ AceGUIDropdown ]]
+---@param trueOrFalse boolean
+function AceGUIDropdown:SetDisabled(trueOrFalse); end
+
+---@param option number
+function AceGUIDropdown:SetValue(selectedOption) end;
+
+---@param DropDownItems table
+function AceGUIDropdown:SetList(DropDownItems); end
+
+---@param selectedText string
+function AceGUIDropdown:SetText(selectedText); end
+
+---@param width number
+function AceGUIDropdown:SetWidth(width); end
+
+---@param trueOrFalse boolean
+function AceGUIDropdown:SetFullWidth(trueOrFalse); end
+
+---@param onselectDo function
+function AceGUIDropdown:SetCallBack(onselectDo); end
+
+function AceGUIDropdown:ClearAllPoints(); end
+
+---@type Frame
+AceGUIDropdown.frame = Frame;
+
+--[[ AceGUIEditBox ]]
+---@param trueOrFalse boolean
+function AceGUIEditBox:SetFullWidth(trueOrFalse); end
+
+---@param trueOrFalse boolean
+function AceGUIEditBox:DisableButton(trueOrFalse); end
+
+---@param trueOrFalse boolean
+function AceGUIEditBox:SetDisabled(trueOrFalse); end
+
+---@param width number
+function AceGUIEditBox:SetWidth(width); end
+
+---@param height number
+function AceGUIEditBox:SetHeight(height); end
+
+---@param selectedText string
+function AceGUIEditBox:SetText(selectedText); end
+
+---@return string
+function AceGUIEditBox:GetText(); end
+
+---@param onChangeDo function
+function AceGUIEditBox:SetCallBack(onChangeDo); end
+
+---@param label string
+function AceGUIEditBox:SetLabel(label); end
+
+---@param max number 0 = Limitless
+function AceGUIEditBox:SetMaxLetters(max); end
+
+function AceGUIEditBox:ClearAllPoints(); end
+
+---@type Frame
+AceGUIEditBox.frame = Frame;
+
+--[[ AceGUIMultiLineEditBox ]]
+---@param trueOrFalse boolean
+function AceGUIMultiLineEditBox:SetFullWidth(trueOrFalse); end
+
+---@param trueOrFalse boolean
+function AceGUIMultiLineEditBox:DisableButton(trueOrFalse); end
+
+---@param width number
+function AceGUIMultiLineEditBox:SetWidth(width); end
+
+---@param height number
+function AceGUIMultiLineEditBox:SetHeight(height); end
+
+---@param selectedText string
+function AceGUIMultiLineEditBox:SetText(selectedText); end
+
+---@return string
+function AceGUIMultiLineEditBox:GetText(); end
+
+---@param onChangeDo function
+function AceGUIMultiLineEditBox:SetCallBack(onChangeDo); end
+
+---@param label string
+function AceGUIMultiLineEditBox:SetLabel(label); end
+
+---@param lines number
+function AceGUIMultiLineEditBox:SetNumLines(lines); end
+
+---@param max number 0 = Limitless
+function AceGUIMultiLineEditBox:SetMaxLetters(max); end
+
+function AceGUIMultiLineEditBox:ClearAllPoints(); end
+
+---@type Frame
+AceGUIMultiLineEditBox.frame = Frame;
+
+--[[ AceGUIIcon ]]
+
+---@param width number
+function AceGUIIcon:SetWidth(width); end
+
+---@param height number
+function AceGUIIcon:SetHeight(height); end
+
+---@param width number
+---@param height number
+function AceGUIIcon:SetImageSize(width, height); end
+
+---@param location string
+function AceGUIIcon:SetImage(location); end
+
+function AceGUIIcon:ClearAllPoints(); end
+
+---@type Frame
+AceGUIIcon.frame = Frame;
+
+---@type Frame
+AceGUILabel.frame = {};
+
 ---@param min number
 ---@param max number|nil
 function RandomRoll(min, max) end
