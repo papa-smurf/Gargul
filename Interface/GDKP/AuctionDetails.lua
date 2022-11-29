8ba4ceacb4cb728325fa7e868fcd7fa5f7551207
Kevin Op den Kamp <k.opdenkamp@gmail.com>
2022-11-29 09:39:58 +0100

    Revamped GDKP auction details window

--- Interface/GDKP/AuctionDetails.lua
+++ Interface/GDKP/AuctionDetails.lua
@@ -4,20 +4,23 @@ local _, GL = ...;
 GL.AceGUI = GL.AceGUI or LibStub("AceGUI-3.0");
 local AceGUI = GL.AceGUI;
 
+---@type Interface
+local Interface = GL.Interface;
+
+---@type Data
+local Constants = GL.Data.Constants;
+
 ---@class AuctionDetails
 GL.Interface.GDKP.AuctionDetails = {}
 
 ---@type AuctionDetails
 local AuctionDetails = GL.Interface.GDKP.AuctionDetails;
 
----@type Interface
-local Interface = GL.Interface;
-
 ---@return void
 function AuctionDetails:draw(session, checksum)
     GL:debug("GDKP.AuctionDetails:draw");
 
-    local VerticalSpacer;
+    local Spacer;
 
     -- Release any existing edit auction window
     self:close();
@@ -39,104 +42,174 @@ function AuctionDetails:draw(session, checksum)
         return;
     end
 
+    local GDKPOverviewFrame = Interface:get(Overview, "GDKPOverview").frame;
+
+    -- This entry should always exist, if it doesn't something went wrong (badly)
+    local ItemEntry = GL.DB.Cache.ItemsByID[tostring(Auction.itemID)];
+
+    if (GL:empty(ItemEntry)) then
+        return;
+    end
+
     -- Create a container/parent frame
     local Window = AceGUI:Create("InlineGroup");
     Window:SetLayout("Flow");
-    Window:SetWidth(300);
-    Window:SetHeight(280);
-    Window:SetPoint("TOPLEFT", Interface:get(Overview, "GDKPOverview").frame, "TOPRIGHT", 2, 16);
+    Window:SetWidth(260);
+    Window:SetHeight(300);
+    Window:SetPoint("TOPLEFT", GDKPOverviewFrame, "TOPRIGHT", 2, 16);
+    Window:SetPoint("BOTTOMLEFT", GDKPOverviewFrame, "BOTTOMLEFT", 2);
     Interface:set(self, "Window", Window);
     Window.frame:SetFrameStrata("HIGH");
     Window.frame:Show();
 
+    local ScrollFrameHolder = GL.AceGUI:Create("SimpleGroup");
+    ScrollFrameHolder:SetLayout("FILL")
+    ScrollFrameHolder:SetFullWidth(true);
+    ScrollFrameHolder:SetFullHeight(true);
+    Window:AddChild(ScrollFrameHolder);
+
+    local ScrollFrame = GL.AceGUI:Create("ScrollFrame");
+    ScrollFrame:SetLayout("Flow");
+    ScrollFrameHolder:AddChild(ScrollFrame);
+
+    local auctionWasDeleted = not GL:higherThanZero(Auction.price);
+    local concernsManualAdjustment = Auction.itemID == Constants.GDKPPotIncreaseItemID;
+    local itemLabel;
+
+    if (not auctionWasDeleted) then
+        if (concernsManualAdjustment) then
+            itemLabel = string.format("|cFF%s%sg|r added to pot by |c00%s%s|r\nNote: %s",
+                Constants.ClassHexColors.rogue,
+                Auction.price or "0",
+                GL:classHexColor(GL.Player:classByName(Auction.Winner.name, 0), GL.Data.Constants.disabledTextColor),
+                Auction.Winner.name,
+                Auction.note or ""
+            );
+        else
+            itemLabel = string.format(
+    "|cFF%s%s|r paid |cFF%s%sg|r for\n%s",
+                GL:classHexColor(Auction.Winner.class),
+                Auction.Winner.name or "",
+                Constants.ClassHexColors.rogue,
+                Auction.price or "0",
+                ItemEntry.link
+            );
+        end
+
+        -- Item was deleted
+    else
+        local reason = Auction.reason;
+
+        if (GL:empty(reason)) then
+            reason = "-";
+        end
+
+        itemLabel = string.format(
+            "|cFFbe3333Deleted by|r |cFF%s%s|r\nReason: %s",
+            GL:classHexColor(Auction.CreatedBy.class),
+            Auction.CreatedBy.name,
+            reason
+        );
+    end
+
     local ItemLink = AceGUI:Create("Label");
     ItemLink:SetFontObject(_G["GameFontNormal"]);
     ItemLink:SetFullWidth(true);
-    ItemLink:SetText(string.format(
-        "|cFF%s%s|r paid |cFF%s%sg|r for\n%s",
-        GL:classHexColor(Auction.Winner.class),
-        Auction.Winner.name or "",
-        GL:classHexColor("rogue"),
-        Auction.price or "0",
-        GL.DB.Cache.ItemsByID[tostring(Auction.itemID)].link
-    ));
-    Window:AddChild(ItemLink);
+    ItemLink:SetText(itemLabel);
+    ScrollFrame:AddChild(ItemLink);
 
-    VerticalSpacer = AceGUI:Create("SimpleGroup");
-    VerticalSpacer:SetLayout("FILL");
-    VerticalSpacer:SetFullWidth(true);
-    VerticalSpacer:SetHeight(4);
-    Window:AddChild(VerticalSpacer);
-
-    local DropDownItems = {};
-    local ItemOrder = {};
-
-    local Sessions = GL.DB:get("GDKP.Ledger");
-    table.sort(Sessions, function (a, b)
-        return a.createdAt < b.createdAt;
-    end);
-    for _, Session in pairs(Sessions) do
-        DropDownItems[Session.ID] = Session.title;
-        tinsert(ItemOrder, Session.ID);
+    local AuctionEntry = AceGUI:Create("Label");
+    AuctionEntry:SetText("\n|c00967FD2About|r");
+    AuctionEntry:SetFullWidth(true);
+    ScrollFrame:AddChild(AuctionEntry);
+    for key, val in pairs(Auction or {}) do
+        if (type(val) ~= "table") then
+            AuctionEntry = AceGUI:Create("Label");
+            AuctionEntry:SetText(string.format("%s: %s", key, val));
+            AuctionEntry:SetFullWidth(true);
+            ScrollFrame:AddChild(AuctionEntry);
+        end
     end
 
-    local SessionDropdown = AceGUI:Create("Dropdown");
-    SessionDropdown:SetValue(session);
-    SessionDropdown:SetList(DropDownItems, ItemOrder);
-    SessionDropdown:SetText(DropDownItems[session]);
-    SessionDropdown:SetLabel("Session");
-    SessionDropdown:SetWidth(250);
-    Window:AddChild(SessionDropdown);
+    local WinnerEntry = AceGUI:Create("Label");
+    WinnerEntry:SetText("\n|c00967FD2Won by|r");
+    WinnerEntry:SetFullWidth(true);
+    ScrollFrame:AddChild(WinnerEntry);
 
-    local PlayernameInput = GL.AceGUI:Create("EditBox");
-    PlayernameInput:DisableButton(true);
-    PlayernameInput:SetHeight(20);
-    PlayernameInput:SetWidth(250);
-    PlayernameInput:SetText(Auction.Winner.name);
-    PlayernameInput:SetLabel("Player name");
-    Window:AddChild(PlayernameInput);
-
-    local Checkbox = AceGUI:Create("CheckBox");
-    Checkbox:SetValue(Auction.paid);
-    Checkbox:SetLabel("Paid");
-    Checkbox:SetFullWidth(true);
-    Checkbox.text:SetTextColor(.99, .85, .06);
-    Checkbox.text:SetFontObject(_G["GameFontHighlightSmall"]);
-    Window:AddChild(Checkbox);
-
-    local SaveButton = AceGUI:Create("Button");
-    SaveButton:SetText("Save");
-    SaveButton:SetWidth(100); -- Minimum is 60
-    SaveButton:SetCallback("OnClick", function()
-        local somethingChanged = false;
-        local newName = strtrim(PlayernameInput:GetText());
-
-        -- The winner was changed
-        if (not GL:empty(newName)
-            and Auction.Winner.name ~= newName
-        ) then
-            GL.GDKP:reassignAuction(session, checksum, newName);
+    for key, val in pairs(Auction.Winner or {}) do
+        if (type(val) == "string" and val ~= "nil") then
+            WinnerEntry = AceGUI:Create("Label");
+            WinnerEntry:SetText(string.format("%s: %s", key, val));
+            WinnerEntry:SetFullWidth(true);
+            ScrollFrame:AddChild(WinnerEntry);
         end
+    end
 
-        -- The session was changed (make sure we do this last!)
-        if (session ~= SessionDropdown:GetValue()) then
-            GL.GDKP:moveAuction(checksum, session, SessionDropdown:GetValue());
+    local CreatedByEntry = AceGUI:Create("Label");
+    CreatedByEntry:SetText("\n|c00967FD2Created by|r");
+    CreatedByEntry:SetFullWidth(true);
+    ScrollFrame:AddChild(CreatedByEntry);
+
+    for key, val in pairs(Auction.CreatedBy or {}) do
+        if (type(val) == "string" and val ~= "nil") then
+            CreatedByEntry = AceGUI:Create("Label");
+            CreatedByEntry:SetText(string.format("%s: %s", key, val));
+            CreatedByEntry:SetFullWidth(true);
+            ScrollFrame:AddChild(CreatedByEntry);
         end
+    end
 
-        -- The paid status was changed
-        ---@todo: implement
+    if (type(Auction.Bids) == "table") then
+        local BidEntry = AceGUI:Create("Label");
+        BidEntry:SetText("\n|c00967FD2Bids|r");
+        BidEntry:SetFullWidth(true);
+        ScrollFrame:AddChild(BidEntry);
 
+        local firstBid = true;
+        local linebreak = "";
+        for player, Bid in pairs(Auction.Bids or {}) do
+            if (not firstBid) then
+                linebreak = "\n";
+            end
+
+            BidEntry = AceGUI:Create("Label");
+            BidEntry:SetText(string.format("%sBy: %s", linebreak, player));
+            BidEntry:SetFullWidth(true);
+            ScrollFrame:AddChild(BidEntry);
+
+            for key, val in pairs(Bid or {}) do
+                if (type(val) ~= "table") then
+                    BidEntry = AceGUI:Create("Label");
+                    BidEntry:SetText(string.format("%s: %s", key, val));
+                    BidEntry:SetFullWidth(true);
+                    ScrollFrame:AddChild(BidEntry);
+                else
+                    for bidderKey, bidderVal in pairs(val or {}) do
+                        BidEntry = AceGUI:Create("Label");
+                        BidEntry:SetText(string.format("%s: %s", bidderKey, bidderVal));
+                        BidEntry:SetFullWidth(true);
+                        ScrollFrame:AddChild(BidEntry);
+                    end
+                end
+            end
+
+            firstBid = false;
+        end
+    end
+
+    Spacer = AceGUI:Create("SimpleGroup");
+    Spacer:SetLayout("FILL")
+    Spacer:SetFullWidth(true);
+    Spacer:SetHeight(30);
+    ScrollFrame:AddChild(Spacer);
+
+    local CloseButton = AceGUI:Create("Button");
+    CloseButton:SetText("Close");
+    CloseButton:SetFullWidth(true);
+    CloseButton:SetCallback("OnClick", function()
         self:close();
     end);
-    Window:AddChild(SaveButton);
-
-    local CancelButton = AceGUI:Create("Button");
-    CancelButton:SetText("Cancel");
-    CancelButton:SetWidth(100); -- Minimum is 60
-    CancelButton:SetCallback("OnClick", function()
-        self:close();
-    end);
-    Window:AddChild(CancelButton);
+    ScrollFrame:AddChild(CloseButton);
 end
 
 function AuctionDetails:close()
