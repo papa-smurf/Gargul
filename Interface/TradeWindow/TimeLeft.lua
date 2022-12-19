0ca91646cbb133ad4edc0444ff101a86b7f5d0c5
Kevin Op den Kamp <k.opdenkamp@gmail.com>
2022-12-19 16:28:34 +0100

    Double clicking trade timers now trades the item, either to the active trade window or your selected target

--- Interface/TradeWindow/TimeLeft.lua
+++ Interface/TradeWindow/TimeLeft.lua
@@ -223,13 +223,13 @@ function TimeLeft:createHotkeyExplanationWindow()
 
     HotKeyExplanation:SetLayout("Fill");
     HotKeyExplanation:SetWidth(210);
-    HotKeyExplanation:SetHeight(74);
+    HotKeyExplanation:SetHeight(78);
     HotKeyExplanation.frame:SetParent(self.Window);
     self:hideExplanationWindow();
 
     local Text = GL.AceGUI:Create("Label");
     Text:SetText(string.format(
-        "\nRoll or auction: |c00a79eff%s|r\nAward: |c00a79eff%s|r\nDisenchant: |c00a79eff%s|r",
+        "\nRoll or auction: |c00a79eff%s|r\nAward: |c00a79eff%s|r\nDisenchant: |c00a79eff%s|r\nTrade: |c00a79effDBLCLK|r",
         GL.Settings:get("ShortcutKeys.rollOffOrAuction"),
         GL.Settings:get("ShortcutKeys.award"),
         GL.Settings:get("ShortcutKeys.disenchant")
@@ -612,8 +612,26 @@ function TimeLeft:refreshBars()
             end
         end);
 
-        -- Close the roll window on rightclick
-        TimerBar:SetScript("OnMouseDown", function(_, mouseButtonPressed)
+        local onDoubleClick = function ()
+            -- Open a trade window with the targeted unit if we don't have one open yet
+            if (not TradeFrame:IsShown()) then
+                if (not UnitIsPlayer("target")) then
+                    return;
+                end
+
+                GL.TradeWindow:open("target", function ()
+                    GL.TradeWindow:addItem(GL:getItemIDFromLink(BagItem.itemLink));
+                end, true);
+
+                return;
+            end
+
+            -- A trade window is open already, just add the item
+            GL.TradeWindow:addItem(GL:getItemIDFromLink(BagItem.itemLink));
+        end;
+
+        local lastClickTime;
+        TimerBar:SetScript("OnMouseUp", function(_, mouseButtonPressed)
             -- The user doesnt want to use shortcut keys when solo
             if (not GL.User.isInGroup
                 and GL.Settings:get("ShortcutKeys.onlyInGroup")
@@ -649,6 +667,16 @@ function TimeLeft:refreshBars()
                     ChatFrameEditBox:Insert(BagItem.itemLink);
                 else
                     ChatEdit_InsertLink(BagItem.itemLink);
+                end
+            else
+                local currentTime = GetTime();
+
+                -- Double click behavior detected
+                if (lastClickTime and currentTime - lastClickTime <= .5) then
+                    onDoubleClick();
+                    lastClickTime = nil;
+                else
+                    lastClickTime = currentTime;
                 end
             end
         end)
