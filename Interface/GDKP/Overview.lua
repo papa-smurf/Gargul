f7e234cef43ebe7323ac0a402e94daff8627e5ca
Kevin Op den Kamp <k.opdenkamp@gmail.com>
2023-03-15 18:30:57 +0100

    Removed Gargul's local item cache and used client's instead for better performance

--- Interface/GDKP/Overview.lua
+++ Interface/GDKP/Overview.lua
@@ -636,7 +636,7 @@ function Overview:refreshLedger()
             local concernsManualAdjustment = Auction.itemID == Constants.GDKP.potIncreaseItemID;
 
             -- This entry should always exist, if it doesn't something went wrong (badly)
-            local ItemEntry = DB.Cache.ItemsByID[tostring(Auction.itemID)];
+            local ItemEntry = GL:getCachedItem(Auction.itemID);
 
             if (GL:empty(ItemEntry)) then
                 break;
