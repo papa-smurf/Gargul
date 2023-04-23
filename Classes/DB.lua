07089002857cf27f971b1580bcb28b08fe0ba4c2
DESKTOP-R8UE7HF\Kevin <k.opdenkamp@gmail.com>
2023-04-23 13:39:07 +0200

    Rewrote Gargul's data storage layer (easier to maintain)

--- Classes/DB.lua
+++ Classes/DB.lua
@@ -1,120 +1,86 @@
 ---@type GL
 local _, GL = ...;
 
+---@type Events
+local Events = GL.Events;
+
 ---@class DB
 GL.DB = {
     _initialized = false,
-    AwardHistory = {},
-    BoostedRolls = {},
-    Cache = {},
-    GDKP = {},
-    LoadDetails = {},
-    LootPriority = {},
-    MinimapButton = {},
-    PlusOnes = {},
-    Settings = {},
-    SoftRes = {},
-    TMB = {},
-    Utility = {},
+
+    Tables = {
+        "AwardHistory",
+        "BoostedRolls",
+        "GDKP",
+        "LoadDetails",
+        "LootPriority",
+        "MinimapButton",
+        "PlusOnes",
+        "Settings",
+        "SoftRes",
+        "TMB",
+        "TradeTime",
+        "Utility",
+    },
 };
 
+---@type DB
 local DB = GL.DB;
 
+---@return void
 function DB:_init()
-    GL:debug("DB:_init");
-
-    -- No need to initialize this class twice
     if (self._initialized) then
         return;
     end
 
+    -- Should only be possible on first boot
     if (not GargulDB or not type(GargulDB) == "table") then
         GargulDB = {};
     end
 
-    -- Prepare our database tables
-    GargulDB.AwardHistory = GargulDB.AwardHistory or {};
-    GargulDB.BoostedRolls = GargulDB.BoostedRolls or {};
-    GargulDB.GDKP = GargulDB.GDKP or {};
-    GargulDB.LoadDetails = GargulDB.LoadDetails or {};
-    GargulDB.LootPriority = GargulDB.LootPriority or {};
-    GargulDB.MinimapButton = GargulDB.MinimapButton or {};
-    GargulDB.PlusOnes = GargulDB.PlusOnes or {};
-    GargulDB.Settings = GargulDB.Settings or {};
-    GargulDB.SoftRes = GargulDB.SoftRes or {};
-    GargulDB.TMB = GargulDB.TMB or {};
-    GargulDB.Utility = GargulDB.Utility or {};
-
-    -- Provide a shortcut for each table
-    self.AwardHistory = GargulDB.AwardHistory;
-    self.BoostedRolls = GargulDB.BoostedRolls;
-    self.GDKP = GargulDB.GDKP;
-    self.LoadDetails = GargulDB.LoadDetails;
-    self.LootPriority = GargulDB.LootPriority;
-    self.MinimapButton = GargulDB.MinimapButton;
-    self.PlusOnes = GargulDB.PlusOnes;
-    self.Settings = GargulDB.Settings;
-    self.SoftRes = GargulDB.SoftRes;
-    self.TMB = GargulDB.TMB;
-    self.Utility = GargulDB.Utility;
+    -- Prepare our saved variables and add a shortcut to each table
+    for _, identifier in pairs(self.Tables or {}) do
+        GargulDB[identifier] = GargulDB[identifier] or {};
+        self[identifier] = GargulDB[identifier];
+    end
 
     -- Fire DB:store before every logout/reload/exit
-    GL.Events:register("DBPlayerLogoutListener", "PLAYER_LOGOUT", self.store);
+    Events:register("DBPlayerLogoutListener", "PLAYER_LOGOUT", function ()
+        self:store();
+    end);
 
     self._initialized = true;
 end
 
---- Make sure the database persists between sessions
---- This is just a safety precaution and should strictly
---- speaking not be necessary, but hey you never know!
-function DB:store()
-    GL:debug("DB:store");
-
-    GargulDB.AwardHistory = GL.DB.AwardHistory;
-    GargulDB.BoostedRolls = GL.DB.BoostedRolls;
-    GargulDB.GDKP = GL.DB.GDKP;
-    GargulDB.LoadDetails = GL.DB.LoadDetails;
-    GargulDB.LootPriority = GL.DB.LootPriority;
-    GargulDB.MinimapButton = GL.DB.MinimapButton;
-    GargulDB.PlusOnes = GL.DB.PlusOnes;
-    GargulDB.Settings = GL.Settings.Active;
-    GargulDB.SoftRes = GL.DB.SoftRes;
-    GargulDB.TMB = GL.DB.TMB;
-    GargulDB.Utility = GL.DB.Utility;
-end
-
--- Get a value from the database, or return a default if it doesn't exist
+---@param keyString string
+---@param default any
+---@return any
 function DB:get(keyString, default)
-    return GL:tableGet(DB, keyString, default);
+    return GL:tableGet(self, keyString, default);
 end
 
--- Set a database value by a given key and value
-function DB:set(keyString, value)
-    return GL:tableSet(DB, keyString, value);
+---@param keyString string
+---@param value any
+---@param ignoreIfExists boolean If the given final key exists then it will not be overwritten
+---@return boolean
+function DB:set(keyString, value, ignoreIfExists)
+    return GL:tableSet(self, keyString, value, ignoreIfExists);
 end
 
--- Add a database value to a given table
+---@param keyString string
+---@param value any
+---@return boolean
 function DB:add(keyString, value)
-    return GL:tableAdd(DB, keyString, value);
+    return GL:tableAdd(self, keyString, value);
 end
 
--- Reset the tables
-function DB:reset()
-    GL:debug("DB:reset");
-
-    self.AwardHistory = {};
-    self.BoostedRolls = {};
-    self.GDKP = {};
-    self.LoadDetails = {};
-    self.LootPriority = {};
-    self.MinimapButton = {};
-    self.PlusOnes = {};
-    self.Settings = {};
-    self.SoftRes = {};
-    self.TMB = {};
-    self.Utility = {};
-
-    GL:success("Tables reset");
+--- Make extra(?) sure the database persists between sessions
+--- This is just a safety precaution and should strictly
+--- speaking not be necessary at all, but hey you never know!
+---
+---@return void
+function DB:store()
+    for _, identifier in pairs(self.Tables or {}) do
+        GargulDB[identifier] = self[identifier];
+    end
 end
-
-GL:debug("DB.lua");
