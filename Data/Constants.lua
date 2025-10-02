5ed00c5c4fa32dc4864536a662e71f28e403833c
papa-smurf <4224975+papa-smurf@users.noreply.github.com>
2025-10-02 15:21:55 +0200

    Version v7.7.3

--- Data/Constants.lua
+++ Data/Constants.lua
@@ -588,7 +588,7 @@ GL.Data.Constants = {
     ]]
     Comm = {
         channel = "GargulComm2",
-        minimumAppVersion = "7.6.33",
+        minimumAppVersion = "7.7.0",
 
         ---@class CommActions
         Actions = {
